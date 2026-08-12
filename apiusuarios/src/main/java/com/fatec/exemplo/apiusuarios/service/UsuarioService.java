07a74538f69e50b753986beb6568bd741773cdc7
alvesrzz <gabriel.carvalho1524@gmail.com>
2026-08-12 12:21:31 -0300

    .

---
+++ apiusuarios/src/main/java/com/fatec/exemplo/apiusuarios/service/UsuarioService.java
@@ -0,0 +1,24 @@
+package com.fatec.exemplo.apiusuarios.service;
+
+import com.fatec.exemplo.apiusuarios.model.Usuario;
+import com.fatec.exemplo.apiusuarios.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll(){
+        return usuarioRepository.findAll();
+    }
+    public Usuario salvar(Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deletar(Long id){
+        usuarioRepository.deleteById(id);
+    }
+}
